36a4c310a1ec5a7f0f9b0bebe2f16841b2a88647
divyadpsc <54075426+divyadpsc@users.noreply.github.com>
2026-05-01 15:48:57 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+//optimised o(n)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+             if(mp.find(target-nums[i])!=mp.end()){
+               return {mp[target-nums[i]],i};
+
+             }
+             mp[nums[i]]=i;
+        }
+        
+        return {};
+            
+    }
+};
+
